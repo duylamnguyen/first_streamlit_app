45b2beb32b178417f3d9dff1128b0ccfb9d54634
Duy Nguyen <49040840+duylamnguyen@users.noreply.github.com>
2023-01-15 10:35:40 +0700

    Update streamlit_app.py
    reset index to fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ streamlit.text("🥑🍞Hard-Boiled Free-Range Egg")
    
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+my_fruit_list = my_fruit_list.set_index('Fruit')
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-streamlit.dataframe(my_fruit_list)
+# Display the table on the page.
